b066a599485755a6d3ec0c0f6e176542b3b4019a
jonturner53 <your_email@youremail.com>
2014-06-27 12:44:05 -0500

    adding stdinc

---
+++ cpp/include/stdinc.h
@@ -0,0 +1,43 @@
+/** @file stdinc.h
+ *
+ *  @author Jon Turner
+ *  @date 2011
+ *  This is open source software licensed under the Apache 2.0 license.
+ *  See http://www.apache.org/licenses/LICENSE-2.0 for details.
+ */
+
+#ifndef STDINC_H
+#define STDINC_H
+
+#include <iostream>
+#include <sstream>
+#include <stdint.h>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cassert>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <poll.h>
+#include <limits.h>
+
+#ifdef linux
+#include <linux/sockios.h>
+#endif
+
+#endif
